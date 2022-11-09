eda84f462f3b1ae7aabb93c653c045ec53ddb6ad
Harald Brunner <brunnerh@users.noreply.github.com>
2022-11-09 22:22:06 +0100

    Set headers in preview as well.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import pkg from './package.json';
 
+/**
+ * Headers for enabling access to {@link SharedArrayBuffer}.  
+ * Required for FFMPEG.
+ */
+const headers = {
+	'Cross-Origin-Embedder-Policy': 'require-corp',
+	'Cross-Origin-Opener-Policy': 'same-origin',
+};
+
 export default defineConfig(config => ({
 	base: config.mode == 'production' ? '/screen-recorder/' : '/',
 	define: {
@@ -10,12 +19,8 @@ export default defineConfig(config => ({
 	build: {
 		outDir: 'out',
 	},
-	server: {
-		headers: {
-			'Cross-Origin-Embedder-Policy': 'require-corp',
-			'Cross-Origin-Opener-Policy': 'same-origin',
-		},
-	},
+	server: { headers },
+	preview: { headers },
 	plugins: [
 		svelte({ configFile: 'svelte.config.js' }),
 	],
